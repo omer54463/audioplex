de813f6e8925fc81e1c5429aa394e3e8e66c8b35
omer54463 <omer@lubin.ws>
2023-04-04 23:14:28 +0300

    REMOVE shit

--- audioplex/src/audio/devices/device_notification_callback.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) trait DeviceNotificationCallback {
-    fn on_add(&self, _device_id: String) {}
-    fn on_remove(&self, _device_id: String) {}
-    fn on_state_change(&self, _device_id: String) {}
-    fn on_name_change(&self, _device_id: String) {}
-    fn on_icon_change(&self, _device_id: String) {}
-    fn on_description_change(&self, _device_id: String) {}
-}
--- audioplex/src/audio/devices/mod.rs
+++ audioplex/src/audio/devices/mod.rs
@@ -1,6 +1,4 @@
 pub(crate) mod device;
 pub(crate) mod device_collection;
 pub(crate) mod device_enumerator;
-pub(crate) mod device_notification_callback;
-pub(crate) mod device_notification_client;
 pub(crate) mod device_state;
